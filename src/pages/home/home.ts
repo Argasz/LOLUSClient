9c16c7665eb5862a61916023eb5ef9e575b21b4c
argas <argasz@gmail.com>
2018-04-21 17:10:28 +0200

    Theming, förändringar i scss/html ej färdigt

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import 'rxjs/add/operator/map';
-import {Http} from '@angular/http';
 
 @Component({
   selector: 'page-home',
@@ -9,17 +8,11 @@ import {Http} from '@angular/http';
 })
 export class HomePage {
 
-    posts: any;
-    hello: string;
+  constructor(public navCtrl: NavController) {
 
-  constructor(public navCtrl: NavController, public http: Http) {
-
-      this.http.get('https://www.reddit.com/r/gifs/new/.json?limit=10').map(res => res.json()).subscribe(data => {
+     /* this.http.get('https://www.reddit.com/r/gifs/new/.json?limit=10').map(res => res.json()).subscribe(data => {
         this.posts = data.data.children;
-    });
-      this.http.get('https://pvt.dsv.su.se/Group07/hello').map(helloRes => helloRes.text()).subscribe(text => {
-        this.hello = text.toString();
-      });
+    });*/
   }
 
 }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
-import { HelloPage } from "../hello/hello";
 
 @Component({
   templateUrl: 'tabs.html'
@@ -13,7 +12,6 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
-  tab4Root = HelloPage;
 
   constructor() {
 
