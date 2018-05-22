5c2ef0ec94776d33bb935291f9302dc824d3fa74
Arvid <peldans@gmail.com>
2018-05-22 17:20:30 +0200

    fixed "more" view on police events

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { Hmodal2Component } from './hmodal2/hmodal2';
+@NgModule({
+	declarations: [Hmodal2Component],
+	imports: [],
+	exports: [Hmodal2Component]
+})
+export class ComponentsModule {}
