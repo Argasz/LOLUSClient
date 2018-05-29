633bbeb77988526d6af1bb4f0ab21fc5fa6c86c9
Aron Gassilewski <argasz@gmail.com>
2018-05-29 17:06:30 +0200

    Modal kommer upp när man klickar på markörer på kartan. Kan ej interagera med kartan i modal.

--- src/components/google-map/google-map.ts
+++ src/components/google-map/google-map.ts
@@ -1,9 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
 import { google } from 'google-maps';
-import { Platform } from 'ionic-angular';
+import { Platform, ModalController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation'
 import { Events } from 'ionic-angular';
 import * as firebase from 'firebase/app';
+import { HmodalComponent } from "../hmodal/hmodal";
+import {_} from 'underscore';
 
 @Component({
   selector: 'google-map',
@@ -13,12 +15,15 @@ import * as firebase from 'firebase/app';
 export class GoogleMapComponent {
 
     @ViewChild("map") mapElement;
-     map: any;
+     map: google.maps.Map;
      eventMarkers: Array<google.maps.Marker>;
+     modalCtrl: ModalController;
 
-  constructor(private platform: Platform, private geolocation: Geolocation, public events: Events) {
+
+  constructor(private platform: Platform, private geolocation: Geolocation, public events: Events, private modCtrl: ModalController) {
     let curLocation;
     this.eventMarkers = [];
+    this.modalCtrl = modCtrl;
     platform.ready().then(async () => {
       await geolocation.getCurrentPosition().then(pos =>{
         let ic = {
@@ -52,10 +57,10 @@ export class GoogleMapComponent {
     });
 
     events.subscribe('modal:close', ()=>{
-
       geolocation.getCurrentPosition().then(pos=>{
         this.map.panTo(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      })
+      });
+      this.map.setClickableIcons(true);
     });
 
     events.subscribe('updating:finished', ev => {
@@ -65,8 +70,16 @@ export class GoogleMapComponent {
         this.eventMarkers = [];
         for(let e in ev){
           let latLng = new google.maps.LatLng(ev[e].lat, ev[e].lng);
-          this.setMarker(latLng, ev[e].title + '\nTid:' + ev[e].time + '\nDatum: ' + ev[e].date, ev[e].type);
-
+          let marker = new google.maps.Marker({position: latLng, title: ev[e].title+'\nTid: '+ev[e].time+
+                                              '\nDatum: '+ev[e].date, map:this.map,label:ev[e].type.substr(0,1)});
+          let out = this;
+          marker.addListener('click', function(){
+            console.log('listen');
+            let mod = out.modalCtrl.create(HmodalComponent,{title:ev[e].title, lat:ev[e].lat, lng:ev[e].lng,time:ev[e].time,date:ev[e].date,type:ev[e].type});
+            mod.present();
+          });
+          this.eventMarkers.push(marker);
+          //this.setMarker(latLng, ev[e].title + '\nTid:' + ev[e].time + '\nDatum: ' + ev[e].date, ev[e].type);
         }
     })
   }
